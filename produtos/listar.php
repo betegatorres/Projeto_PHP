182981f3236bf76487e742d3e1b51823cc260fb2
betegatorres <betegatorres@gmail.com>
2026-08-20 12:10:03 -0300

    Aula 4 — Sessão de Usuário e Proteção das Páginas

--- produtos/listar.php
+++ produtos/listar.php
@@ -1,4 +1,10 @@
-<?php
+<?php include '../conexao.php';?>
+<?php include 'verifica_login.php'; ?>
+<?php include 'cabecalho.php'; ?>
 
+<main>
+    <p>Bem-vindo(a), <?php echo $_SESSION['usuario_nome']; ?>!</p>
+    <!-- conteúdo da página -->
+</main>
 
-?>
+<?php include '../rodape.php'; ?>
